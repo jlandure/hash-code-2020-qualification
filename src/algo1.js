769e6952a2fde10c0d85d6d5f03665469f6caf88
Benjamin Petetot <bpetetot@gmail.com>
2020-02-20 20:24:44 +0100

    stats

--- src/algo1.js
+++ src/algo1.js
@@ -1,16 +1,30 @@
 const flatten = require('lodash/flatten')
 const time = []
 
+function stats (problem, field) {
+  const values = problem.libraries.map(lib => lib[field])
+  values.sort((a, b) => a - b);
+  let lowMiddle = Math.floor((values.length - 1) / 2);
+  let highMiddle = Math.ceil((values.length - 1) / 2);
+  let median = (values[lowMiddle] + values[highMiddle]) / 2;
+  let sum = values.reduce((previous, current) => current += previous);
+  let avg = sum / values.length;
+  console.log({ lowMiddle, highMiddle, median, avg, min: Math.min(...values), max: Math.max(...values) })
+}
+
 function main(problem, file) {
   let nextSignUpDay = 0;
   let waitingSignup = -1;
   let nextLibrary = 0
   
   let libraryOpens = [];
+  // console.log(problem)
+  stats(problem, 'nbooks')
+  
 
   for (let day = 0; day < problem.ndays; day++) {
     // signup process
-    if (nextSignUpDay === day && nextLibrary < problem.nlibraries) {
+    if (nextSignUpDay === day && nextLibrary < problem.libraries.length) {
       const library = problem.libraries[nextLibrary];
       nextSignUpDay = day + library.signupDuration; 
       if (waitingSignup !== -1) {
@@ -22,17 +36,20 @@ function main(problem, file) {
 
     // select book process
     libraryOpens.forEach(lib => {
-      problem.libraries[lib].scannedBooks = [...new Set([].concat(problem.libraries[lib].scannedBooks || []).concat(problem.libraries[lib].books))]
+      if (!problem.libraries[lib].scannedBooks) {
+        problem.libraries[lib].scannedBooks = problem.libraries[lib].books
+      }
+      // problem.libraries[lib].scannedBooks = [...new Set([].concat(problem.libraries[lib].scannedBooks || []).concat(problem.libraries[lib].books))]
     })
 
-    console.log({
-      day, 
-      nextSignUpDay,
-      waitingSignup,
-      libraryOpens,
-      nextLibrary,
-      scanned: libraryOpens.map(lib => problem.libraries[lib].scannedBooks),
-    })
+    // console.log({
+    //   day, 
+    //   nextSignUpDay,
+    //   waitingSignup,
+    //   libraryOpens,
+    //   nextLibrary,
+    //   scanned: libraryOpens.map(lib => problem.libraries[lib].scannedBooks),
+    // })
   }
   
   // basic output 
