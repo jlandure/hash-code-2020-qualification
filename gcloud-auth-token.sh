e4feb3febc82ac330e7c44c924fb933719e2d87a
Julien Landuré <julien.landure@gmail.com>
2020-02-20 18:46:15 +0100

    :sparkles:

--- gcloud-auth-token.sh
+++ gcloud-auth-token.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 
-MSYS_NO_PATHCONV=1 docker run --rm -v $(pwd)/.gcloud:/root/.config/gcloud google/cloud-sdk gcloud auth application-default print-access-token
+#MSYS_NO_PATHCONV=1 docker run --rm -v $(pwd)/.gcloud:/root/.config/gcloud google/cloud-sdk gcloud auth application-default print-access-token
+gcloud auth application-default print-access-token
