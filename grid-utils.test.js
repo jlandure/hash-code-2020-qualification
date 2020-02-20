4bce896d00b171e8877098220c1df34ff1e7e5a6
Julien Landuré <julien.landure@gmail.com>
2020-02-20 18:03:34 +0100

    tayo!

--- grid-utils.test.js
+++ grid-utils.test.js
@@ -7,7 +7,10 @@ const gridUtils = require("./grid-utils");
 describe("grid utils", function() {
   describe("cells", function() {
     it("returns all cells of the grid, with coordinates", function() {
-      const cells = gridUtils.cells([[1, 2, 3], [4, 5, 6]]);
+      const cells = gridUtils.cells([
+        [1, 2, 3],
+        [4, 5, 6]
+      ]);
       assert.deepEqual(cells, [
         { x: 0, y: 0, value: 1 },
         { x: 1, y: 0, value: 2 },
@@ -22,8 +25,12 @@ describe("grid utils", function() {
   describe("each", function() {
     it("iterates", function() {
       const iterated = [];
-      gridUtils.forEach([[1, 2], [3, 4]], (value, x, y) =>
-        iterated.push({ x, y, value })
+      gridUtils.forEach(
+        [
+          [1, 2],
+          [3, 4]
+        ],
+        (value, x, y) => iterated.push({ x, y, value })
       );
       assert.deepEqual(iterated, [
         { x: 0, y: 0, value: 1 },
@@ -41,23 +48,44 @@ describe("grid utils", function() {
     });
 
     it("transforms every value in the grid using f", function() {
-      const grid = [[1, 2], [3, 4]];
+      const grid = [
+        [1, 2],
+        [3, 4]
+      ];
       const newGrid = gridUtils.map(grid, i => i + 1);
-      assert.deepEqual(newGrid, [[2, 3], [4, 5]]);
+      assert.deepEqual(newGrid, [
+        [2, 3],
+        [4, 5]
+      ]);
     });
   });
 
   describe("transpose", function() {
     it("transposes a square", function() {
-      assert.deepEqual(gridUtils.transpose([[1, 2], [3, 4]]), [[1, 3], [2, 4]]);
+      assert.deepEqual(
+        gridUtils.transpose([
+          [1, 2],
+          [3, 4]
+        ]),
+        [
+          [1, 3],
+          [2, 4]
+        ]
+      );
     });
 
     it("transposes a rectangle", function() {
-      assert.deepEqual(gridUtils.transpose([[1, 2, 3], [4, 5, 6]]), [
-        [1, 4],
-        [2, 5],
-        [3, 6]
-      ]);
+      assert.deepEqual(
+        gridUtils.transpose([
+          [1, 2, 3],
+          [4, 5, 6]
+        ]),
+        [
+          [1, 4],
+          [2, 5],
+          [3, 6]
+        ]
+      );
     });
   });
 });
